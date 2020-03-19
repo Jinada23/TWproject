1b5047570e8fb66ad5aeaaad5a8d50a8a183962b
Geogre Dumbrava <rewolutionlife@gmail.com>
2020-03-19 09:38:38 +0200

    Project On

---
+++ BusinessLogic/SessionBL.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.User;
+using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+        public ULoginResp UserLogin(ULoginData data)
+        {
+            return new ULoginResp { Status = true };
+        }
+
+
+    }
+}
---
+++ eUseControl.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace eUseControl.Web.App_Start
+
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bootstrap css
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            //bootstrap js
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            
+            //jquery
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-3.4.1.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
